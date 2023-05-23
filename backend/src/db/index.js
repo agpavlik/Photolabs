b0a73d7b8e22f63f89924e185575b0ce7b78609f
agpavlik <agpavlik@gmail.com>
2023-05-23 17:35:26 -0400

    Fix backend/application.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,7 +1,13 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
+  //connectionString: process.env.DATABASE_URL || "",
+  host: process.env.PGHOST,
+  name: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
